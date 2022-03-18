bc5822fceb26d17917501c1c36ed5abaa4eeeafc
haurog <36535774+haurog@users.noreply.github.com>
2022-03-18 22:45:14 +0100

    Moved WETH and DAI to account1 (tx still failing)

--- scripts/single-swap-script.js
+++ scripts/single-swap-script.js
@@ -4,7 +4,9 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 var addresses = require("./addresses")
+require("./transferERC20")
 const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -19,16 +21,91 @@ async function main() {
   const singleSwap = await SingleSwap.deploy(addresses.SWAP_ROUTER);
 
   await singleSwap.deployed();
+  console.log("SingleSwap deployed to:", singleSwap.address);
+
+  const accounts = await ethers.getSigners();
+
+  ///////////////Send WETH//////////////////////////////////////////////////////////////////////
+  const IERC20_SOURCE = "@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20";
+
+  const WETH_WHALE = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+  const WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 
   await hre.network.provider.request({
     method: "hardhat_impersonateAccount",
-    params: [addresses.DAI_WHALE],
+    params: [WETH_WHALE],
   });
-  const impersonatedAccount = await ethers.getSigner(addresses.DAI_WHALE)
+  let signer = await ethers.provider.getSigner(WETH_WHALE);
+  signer.address = signer._address;
+  wethContract = await hre.ethers.getContractAt(IERC20_SOURCE, WETH_ADDRESS, signer);
+  wethContract = wethContract.connect(signer);
 
-  console.log("SingleSwap deployed to:", singleSwap.address);
-  tx = await singleSwap.connect(impersonatedAccount).swapExactOutputSingle(1,10000);
+  console.log("Signer WETH balance", ethers.utils.formatEther(await wethContract.balanceOf(signer.address)))
+  // Signer WETH balance BigNumber { value: "443798472050547768667" }
+
+  await wethContract.transfer(accounts[1].address, ethers.utils.parseEther("10"));
+  balance = await wethContract.balanceOf(accounts[1].address);
+  console.log("WETH balance: ", ethers.utils.formatEther(balance))
+
+  ///////////////Send DAI//////////////////////////////////////////////////////////////////////
+
+   await hre.network.provider.request({
+     method: "hardhat_impersonateAccount",
+     params: [addresses.DAI_WHALE],
+   });
+   signer = await ethers.provider.getSigner(addresses.DAI_WHALE);
+   signer.address = signer._address;
+   daiContract = await hre.ethers.getContractAt(IERC20_SOURCE, addresses.DAI, signer);
+   daiContract = daiContract.connect(signer);
+
+   console.log("Signer DAI balance", ethers.utils.formatEther(await daiContract.balanceOf(signer.address)))
+   // Signer WETH balance BigNumber { value: "443798472050547768667" }
+
+   await daiContract.transfer(accounts[1].address, ethers.utils.parseEther("123456"));
+   balance = await daiContract.balanceOf(accounts[1].address);
+   console.log("DAI balance: ", ethers.utils.formatEther(balance))
+
+  ////////Send ETH/////////////////////////////////////////////////////////////////////////////
+  balance = await ethers.provider.getBalance(accounts[1].address);
+  console.log("balance before: ", ethers.utils.formatEther(balance));
+
+  await signer.sendTransaction({
+    to: accounts[1].address,
+    value: ethers.utils.parseEther("1.0"), // Sends exactly 1.0 ether
+  });
+
+
+
+  balance = await ethers.provider.getBalance(accounts[1].address);
+  console.log("balance after: ", ethers.utils.formatEther(balance));
+
+
+  //////set Eth///////////////////////////////////////////////////////////////////////////////
+
+
+  // await hre.network.provider.request({
+  //   method: "hardhat_setBalance",
+  //   params: ['0x70997970C51812dc3A010C7d01b50e0d17dc79C8', "0x1000"]
+  // });
+
+  // balance = await ethers.provider.getBalance(accounts[1].address);
+  // console.log("balance set: ", balance.toString());
+
+
+  //////Check all accounts///////////////////////////////////////////////////////////////////////////////
+
+
+  for (const account of accounts) {
+    balance = await ethers.provider.getBalance(account.address);
+    console.log(account.address, "has balance", ethers.utils.formatEther(balance));
+  }
+
+  ///////swap //////////////////////////////////////////////////////////////////////////////
+
+  tx = await singleSwap.connect(accounts[1]).swapExactOutputSingle(1,10000);
   console.log(tx);
+
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
