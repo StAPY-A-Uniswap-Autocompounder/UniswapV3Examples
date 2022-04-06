a13576d5dff32003ac0dac4976ea8768c6368b70
danceratopz <danceratopz@gmail.com>
2022-04-06 09:37:24 +0200

    fix: approve single-swap contract in script before dai transfer

--- scripts/single-swap-script.js
+++ scripts/single-swap-script.js
@@ -8,6 +8,17 @@ const hre = require("hardhat");
 const { ethers } = require("hardhat");
 const loadUpERC20 = require("./loadUpERC20.js");
 
+function log_balances(address, eth_balance, dai_balance, weth_balance) {
+  let eth_balance_f = ethers.utils.formatEther(eth_balance);
+  let dai_balance_f = ethers.utils.formatEther(dai_balance);
+  let weth_balance_f = ethers.utils.formatEther(weth_balance);
+  eth_balance_f = (+eth_balance_f).toFixed(4);
+  dai_balance_f = (+dai_balance_f).toFixed(4);
+  weth_balance_f = (+weth_balance_f).toFixed(4);
+  console.log(address, ":", eth_balance_f, "ETH, ", dai_balance_f, "DAI, ", weth_balance_f, "WETH");
+}
+
+
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
@@ -30,16 +41,47 @@ async function main() {
 
   //////Check all accounts///////////////////////////////////////////////////////////////////////////////
 
-
-  for (const account of accounts) {
-    balance = await ethers.provider.getBalance(account.address);
-    console.log(account.address, "has balance", ethers.utils.formatEther(balance));
-  }
+  // for (const account of accounts) {
+  //   balance = await ethers.provider.getBalance(account.address);
+  //   console.log(account.address, "has balance", ethers.utils.formatEther(balance));
+  // }
 
   ///////swap //////////////////////////////////////////////////////////////////////////////
 
-  // tx = await singleSwap.connect(accounts[1]).swapExactOutputSingle(1,10000);
+  signer = await ethers.provider.getSigner(accounts[1].address);
+  const IERC20_SOURCE = "@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20";
+  daiContract = await hre.ethers.getContractAt(IERC20_SOURCE, addresses.DAI, signer);
+  wethContract = await hre.ethers.getContractAt(IERC20_SOURCE, addresses.weth9, signer);
+
+  let eth_balance = await ethers.provider.getBalance(accounts[1].address);
+  let dai_balance = await daiContract.balanceOf(accounts[1].address);
+  let weth_balance = await wethContract.balanceOf(accounts[1].address);
+  log_balances(accounts[1].address, eth_balance, dai_balance, weth_balance);
+
+  let dai_amount = ethers.utils.parseUnits("3456.0", daiContract.decimals);
+
+  console.log("Approving singleSwap for daiContract...")
+  daiContract = daiContract.connect(signer);
+  tx = await daiContract.approve(singleSwap.address, dai_amount);
   // console.log(tx);
+
+  console.log("Calling swapExactInputSingle..")
+  tx = await singleSwap.connect(accounts[1]).swapExactInputSingle(dai_amount);
+  console.log(tx);
+
+  eth_balance = await ethers.provider.getBalance(accounts[1].address);
+  dai_balance = await daiContract.balanceOf(accounts[1].address);
+  weth_balance = await wethContract.balanceOf(accounts[1].address);
+
+  log_balances(accounts[1].address, eth_balance, dai_balance, weth_balance);
+
+  // weth_amount =
+  // dai_amount = .. be sure to include a little bit extra to account for variance
+  // console.log("Calling swapExactOutputSingle..")
+  // tx = await singleSwap.connect(accounts[1]).swapExactOutputSingle(weth_amount, dai_amount);
+  // console.log(tx);
+
+  // log_balances(accounts[1].address, eth_balance, dai_balance, weth_balance);
 
 
 }
