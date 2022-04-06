97d284fb4c43ce6ed9ca10ebb33444234a79e874
danceratopz <danceratopz@gmail.com>
2022-04-06 09:18:00 +0200

    chore: update blocknumber used in hardhat fork

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,7 +15,7 @@ module.exports = {
     hardhat: {
       forking: {
         url: process.env.ALCHEMY_MAINNET_RPC_URL,
-        blockNumber: 14404386  // March 17th 2022, around 13:50 UTC
+        blockNumber: 14530590  // April 6th 2022, around 05:52 UTC
       }
     }
   },
